007adb6e9b3e1974d81ff5320e60d4b1b16ac564
Brian Bansenauer <brianb@cascadia.edu>
2015-05-04 11:51:25 -0700

    Initial Commit

---
+++ MyContactList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyContactList
+//
+//  Created by Brian Bansenauer on 5/4/15.
+//  Copyright (c) 2015 Cascadia College. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
